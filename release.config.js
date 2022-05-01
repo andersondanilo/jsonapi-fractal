21bfa9ee1676d3d14dedc4bcca90b6a1172b6de9
Anderson Danilo <contact@andersondanilo.com>
2022-04-30 21:18:21 -0300

    chore: fix release

--- release.config.js
+++ release.config.js
@@ -20,6 +20,7 @@ module.exports = {
       "@semantic-release/git",
       {
         assets: ["CHANGELOG.md", "package.json", "docs"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       },
     ],
     "@semantic-release/github",
